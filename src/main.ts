8ce8d47afe08c8938746f3bd464cb93bb8739f41
ndeast <nik@ndeast.com>
2023-01-13 15:53:22 -0500

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function sayMyName(name: string): void {
+  if (name === 'Heisenberg') {
+    console.log("You're right");
+  } else {
+    console.log("You're wrong");
+  }
+}
+
+
+sayMyName("Heisenberg");
